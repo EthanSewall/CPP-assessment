a85c3fcf39183a4a78c79c87ec0c503ded6a4427
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-12-02 13:59:22 -0800

    begin tictactoe program

---
+++ tictactoegame/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>;
+
+int main()
+{
+	char grid[3][3] = { '_', '_','_','_','_','_','_','_','_'};
+
+	bool gameFinish = false;
+	bool validInput = false;
+	char inputChar1 = '\0';
+	char inputChar2 = '\0';
+	int row = 0;
+	int column = 0;
+
+	std::cout << grid[0][0] << " " << grid[0][1] << " " << grid[0][2] << std::endl;
+	std::cout << grid[1][0] << " " << grid[1][1] << " " << grid[1][2] << std::endl;
+	std::cout << grid[2][0] << " " << grid[2][1] << " " << grid[2][2] << std::endl;
+
+	while (!gameFinish)
+	{
+
+	}
+
+	return 1;
+}
