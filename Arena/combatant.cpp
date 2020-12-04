4ad15b199b073ece4bb51612baf7f02740d61604
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-12-04 12:54:52 -0800

    arena is the fix

--- Arena/combatant.cpp
+++ Arena/combatant.cpp
@@ -1,19 +1,15 @@
 #include "combatant.h"
 #include <cstdlib>
-class Combatant::Combatant
-{
-public:
 
 	int health;
 	char identifier;
 
-	int attack()
+	int Combatant::attack()
 	{
 		return(std::rand() / (RAND_MAX / 20));
 	}
 
-	void takeDamage(int damage)
+	void Combatant::takeDamage(int damage)
 	{
 		health -= damage;
 	}
-};
