a72131425d270b5ad7ca4fa384f0b95290317dae
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-12-10 12:53:13 -0800

    seek function now exists

--- binary file editing/main.cpp
+++ binary file editing/main.cpp
@@ -2,55 +2,76 @@
 #include <fstream>
 #include <cstdlib>
 
-struct Info
-{
-	char string[10] = "";
-	int number = 0;
-};
 
 int main()
 {
 	std::fstream file;
-	char input[10] = "";
-	char holder[10] = "";
+	char input[20] = "";
+	char holder[20] = "";
 	bool quit = false;
-	std::cout << "what file should be opened?" << std::endl;
+	int filesize = 0;
+	std::cout << "What file should be opened?" << std::endl;
 	std::cin >> input;
 	file.open(input, std::ios_base::in | std::ios_base::binary | std::ios_base::out);
 	if (file.is_open() && file.good() && !quit)
 	{
-			std::cout << "File opened" << std::endl;
+		std::cout << "File opened." << std::endl;
+		file.seekg(0, std::ios::end);
+		filesize = (int)file.tellg();
+		std::cout << "File size: " << filesize << std::endl;
+		file.seekg(0, std::ios::beg);
 		while (file.is_open() && file.good() && !quit)
 		{
-			file.seekg(0, std::ios::beg);
-
-			std::cout << "input or output mode?";
+			std::cout << "Mode? (read, write, seek, add) \n";
 			std::cin >> input;
 
-			if ((input[0] == 'I' || input[0] == 'i') && input[1] == 'n')
+			if ((input[0] == 'R' || input[0] == 'r') && input[1] == 'e' && input[2] == 'a' && input[3] == 'd')
 			{
-				file.read(input, 10);
-				std::cout << input << std::endl;
+				file.read(holder, 20);
+				std::cout << holder << std::endl;
 			}
-			else if ((input[0] == 'O' || input[0] == 'o') && input[1] == 'u' && input[2] == 't')
+			else if ((input[0] == 'W' || input[0] == 'w') && input[1] == 'r' && input[2] == 'i' && input[3] == 't' && input[4] == 'e')
 			{
 				std::cin >> input;
-				file.write(input, 10);
+				file.write(input, 20);
+			}
+			else if ((input[0] == 'S' || input[0] == 's') && input[1] == 'e' && input[2] == 'e' && input[3] == 'k')
+			{
+				std::cin >> input;
+				int a = ((int)input[0] - 48) * 10;
+				int b = ((int)input[1] - 48);
+
+				if (a + b > filesize)
+				{
+					std::cout << "Cannot seek past the file. \n";
+				}
+				else
+				{
+					file.seekg((a + b), std::ios::beg);
+					std::cout << "Seeking to position " << a + b << std::endl;
+				}
+			}
+			else if ((input[0] == 'Q' || input[0] == 'q') && input[1] == 'u' && input[2] == 'i' && input[3] == 't')
+			{
+				quit = true;
 			}
 			else
 			{
-				std::cout << "invalid command" << std::endl;
+				std::cout << "Invalid command." << std::endl;
 			}
-		}
 
+
+		}
 	}
 	else
 	{
-		std::cout << "file could not be opened" << std::endl;
+		std::cout << "File could not be opened." << std::endl;
+		std::cin >> input;
+		return 0;
 	}
 
 	file.close();
-	std::cout << "file is closed";
+	std::cout << "File has been closed.";
 	std::cin >> input;
 	return 1;
 }
